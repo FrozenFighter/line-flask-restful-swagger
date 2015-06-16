962d32f478e307035107bdf9d089f454ebff4993
aloha1003 <knives1003@gmaiil.com>
2015-06-16 17:42:19 +0800

    修正sql lite db SQL

--- line_authtoken.sql
+++ line_authtoken.sql
@@ -1,4 +1,3 @@
+
 DROP TABLE IF EXISTS "line_authtoken";
 CREATE TABLE "line_authtoken" ("line_id" VARCHAR PRIMARY KEY  NOT NULL ,"email" VARCHAR,"line_authtoken" VARCHAR);
-INSERT INTO "line_authtoken" VALUES('u7aa54fed3cc41756c8446d4d2f7074b2','guru1003@gmail.com','DTfoJFk8HBLf1W9jP782.EQvp7tZef1Birg1nl3b0SG.0J/uqNvr0CYUQHOtudd8iAkO+af5F/N34iWHV57/xpA=',NULL);
-INSERT INTO "line_authtoken" VALUES('u08ebf6545e6d97d934b9fb5d2ec35a81','knives1003@gmail.com','DT9zrZPIAxYFl8IhKAV1.coV2vf5TL9K0UFTDlSnAGq.p+4MYGuzCkFBQJe0sg+c2mdkJLh6HWkgFl+cSjHTTzE=',NULL);
